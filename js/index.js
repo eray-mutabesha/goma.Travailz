03da212230c7070b9487aebd46fc22012e6f49cb
eray-mutabesha <eraymutabesha13@gmail.com>
2023-11-25 21:22:22 +0200

    some changes

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+let h1=document.getElementById("Aventurexplore")
+text ="Aventurexplore";
+let i=0;
+function anim(){
+    h1.innerHTML+=text[i];
+    if(i<text.length -1){
+        i++;
+      
+    }
+   
+    else{
+    i=0;
+    return
+    
+    }
+    setTimeout(anim,100);
+   
+}
+anim()
