ac112d7eb8cb477ff3f475ac7fe5b6620394f8a1
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-04-15 10:28:52 -0700

    apropos

--- js/apropos.js
+++
@@ -1,5 +0,0 @@
-const imgmenu=document.getElementById("icon_menu")
-const menup=document.querySelector(".menu_principale")
-imgmenu.addEventListener("click",()=>{
-    menup.classList.toggle("menu_li")
-})
